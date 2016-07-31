44e3ff2a238ab562dd88dab0c4c7b3a143e2b0d8
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-07-31 17:43:50 +0100

    - Fetching posts from API - DONE - TODO: Add new Post component/route

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,14 +1,52 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 const propTypes = {
-
+  posts: PropTypes.object.isRequired,
 };
 
-function PostsIndex() {
-  return (
-    <div>List of blog posts</div>
-  );
+class PostsIndex extends Component {
+
+  componentWillMount() {
+    console.log('Fetch all the posts here');
+    this.props.fetchPosts();
+  }
+
+  render() {
+    // const posts = this.props.posts.all.map((p) => (
+    //   <li>p</li>
+    // ));
+
+    return (
+      <div>
+        <div>List of blog posts</div>
+        <ul>
+        </ul>
+      </div>
+    );
+  }
+
+
 }
 PostsIndex.propTypes = propTypes;
 
-export default PostsIndex;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    fetchPosts,
+  }, dispatch);
+}
+
+// Rather than taking the mapDispatchToProps function as a parameter.
+// We just pass an object that gathers the action creators which seems
+// to be implicitly wrapped by a dispatch.
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostsIndex);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import reducerPosts from './reducerPosts';
 
 const rootReducer = combineReducers({
-
+  posts: reducerPosts,
 });
 
 export default rootReducer;
